7ce2059c7132bbd2dc66a6c9e5f92a5bdbbd89c1
Sheel Patel <115194165+Kyoko522@users.noreply.github.com>
2023-10-15 05:50:28 -0400

    general file css completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import './index.css'
+
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
 import About from './components/about/About'
