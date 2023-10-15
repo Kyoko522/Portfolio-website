58663e740c7fa5a3b356d0ab84d00e9133a948b1
Sheel Patel <115194165+Kyoko522@users.noreply.github.com>
2023-10-15 12:14:31 -0400

    worked on adding the image to the header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
+import Sheel_Img from '../..//assets/sheel.png'
+import headerSocials from './headerSocials'
 
 export const Header = () => {
   return (
@@ -10,6 +12,11 @@ export const Header = () => {
         <h1>Sheel Patel</h1>
         <h5 className='text-light'>Fullstack Developer and Mobile Developer</h5>
         <CTA />
+        <headerSocials />
+        <div className="sheel_img">
+          <img src={Sheel_Img}/>
+          </div>
+          <a href='#content' className='scroll_down'>Scroll Down</a>
       </div>
     </header>
   )
---
+++ src/components/header/headerSocials.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {BsGithub} from 'react-icons/bs'
+import {BsInstagram} from 'react-icons/bs'
+
+export const headerSocials = () => {
+  return (
+    <div className='header__socials'>
+        <a href="https://ca.linkedin.com/in/sheel-patel-1212a0262" target='_blank'><BsLinkedin/></a>
+        <a href="https://github.com/Kyoko522" target='_blank'><BsGithub/></a>
+        <a href="https://https://www.instagram.com/sheel_52/" target='_blank'><BsInstagram/></a>
+    </div>
+  )
+
+}
+
+export default headerSocials
