c34965f9f01a5c5acf7c77e9bd6a2f43e70626ed
Sheel Patel <115194165+Kyoko522@users.noreply.github.com>
2023-12-17 16:38:25 -0500

    project section done

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,6 +2,23 @@ import React from 'react'
 import './portfolio.css'
 import IMG1 from '../../assets/Flashcards-pic.png'
 
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'FlshCards using python',
+    github: 'http://github.com',
+    demo: 'http://github.com'
+  },
+{
+  id: 2,
+  image: IMG1,
+  title: 'title of the project that i have done',
+  github: 'http://github.com',
+  demo: 'http://github.com'
+}
+]
 export const Portfolio = () => {
   return (
     <section id='portfolio'>
@@ -9,64 +26,25 @@ export const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className='container portfolio__container'>
-      <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a protfolio item title</h3>
-          <a href='#' className='btn btn-primary'>View Code</a>
-          <a href='#' className='btn btn-primary'>Live Demo</a>
-        </article>
-
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a protfolio item title</h3>
-          <a href='#' className='btn btn-primary'>View Code</a>
-          <a href='#' className='btn btn-primary'>Live Demo</a>
-        </article>
-
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a protfolio item title</h3>
-          <a href='#' className='btn btn-primary'>View Code</a>
-          <a href='#' className='btn btn-primary'>Live Demo</a>
-        </article>
-
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a protfolio item title</h3>
-          <a href='#' className='btn btn-primary'>View Code</a>
-          <a href='#' className='btn btn-primary'>Live Demo</a>
-        </article>
-
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a protfolio item title</h3>
-          <a href='#' className='btn btn-primary'>View Code</a>
-          <a href='#' className='btn btn-primary'>Live Demo</a>
-        </article>
-
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a protfolio item title</h3>
-          <a href='#' className='btn btn-primary'>View Code</a>
-          <a href='#' className='btn btn-primary'>Live Demo</a>
-        </article>
-
+        {data.map(({ id, image, title, github, demo }) => (
+          <article className='portfolio__item' key={id}>
+            <div className='portfolio__item-image'>
+              <img src={image} alt={title} />
+            </div>
+            <h3>{title}</h3>
+            <div className='portfolio__item-cta'>
+              <a href={github} className='btn btn-primary'>
+                View Code
+              </a>
+              <a href={demo} className='btn btn-primary'>
+                Live Demo
+              </a>
+            </div>
+          </article>
+        ))}
       </div>
-
     </section>
-  )
+  );
 }
 
 export default Portfolio
