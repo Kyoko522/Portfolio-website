094bf994d55f2fb79c9b949153c693a80c34cd6b
Sheel Patel <115194165+Kyoko522@users.noreply.github.com>
2023-12-17 20:19:58 -0500

    projects added

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,23 +1,47 @@
 import React from 'react'
 import './portfolio.css'
 import IMG1 from '../../assets/portfolio.png'
-
+import IMG2 from '../../assets/icon.png'
+import IMG3 from '../../assets/TaskTonic.png'
+import IMG4 from '../../assets/Minesweeper.png'
+import IMG5 from '../../assets/Flashcards-pic.png'
 
 const data = [
   {
     id: 1,
     image: IMG1,
-    title: 'Perssonal Portfolio',
-    github: 'http://github.com',
-    demo: 'http://github.com'
+    title: 'Personal Portfolio',
+    github: 'https://github.com/Kyoko522/react-portfolio',
+    demo: 'still need to host to the web'
   },
 {
   id: 2,
-  image: IMG1,
-  title: 'title of the project that i have done',
-  github: 'http://github.com',
-  demo: 'http://github.com'
-}
+  image: IMG2,
+  title: '3D Animated Portfolio',
+  github: 'https://github.com/Kyoko522/Sheel-3D-Portfolio/tree/main',
+  demo: 'https://sheel-portfolio.netlify.app/'
+},
+{
+  id: 3,
+  image: IMG3,
+  title: 'TaskTonic',
+  github: 'https://github.com/Kyoko522/TaskTonic',
+  demo: 'https://tasktonic.netlify.app/',
+},
+{
+  id: 4,
+  image: IMG4,
+  title: 'Minesweeper',
+  github: 'https://github.com/Kyoko522/MineSweeper-Game'
+},
+{
+  id: 5, 
+  image: IMG5,
+  title: 'Python Flashcards',
+  github: 'https://github.com/Kyoko522/Flash_Cards'
+},
+
+
 ]
 export const Portfolio = () => {
   return (
@@ -29,7 +53,7 @@ export const Portfolio = () => {
         {data.map(({ id, image, title, github, demo }) => (
           <article className='portfolio__item' key={id}>
             <div className='portfolio__item-image'>
-              <img src={image} alt={title} />
+              <img className='portfolio__project-img' src={image} alt={title} />
             </div>
             <h3>{title}</h3>
             <div className='portfolio__item-cta'>
