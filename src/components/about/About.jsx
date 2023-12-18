9f7d30b6d84bd485abb43a8231b7317926d1a593
Sheel Patel <115194165+Kyoko522@users.noreply.github.com>
2023-12-17 19:26:37 -0500

    experience section done and about me swap photo done

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './about.css'
 import Sheel from '../../assets/sheel.png';
+import Sheel2 from '../../assets/sheel2.png';
+import Sheel3 from '../../assets/sheel3.png';
+import Sheel4 from '../../assets/sheel4.png';
 import {FaAward} from 'react-icons/fa'
 
 export const About = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const images = [Sheel, Sheel2, Sheel3, Sheel4]; // Adding more images
+
+  const handleImageClick = () => {
+    console.log('Image clicked');
+    const nextIndex = (currentImageIndex + 1) % images.length;
+    setCurrentImageIndex(nextIndex);
+  };
+
   return (
     <section id='about'>
       <h5>Get To Know</h5>
@@ -12,9 +24,10 @@ export const About = () => {
 
       <div className='container about__container'>
 
-        <div className= 'about__me'>
+        <div className= 'about__me' onClick={handleImageClick}>
           <div className= 'about__me-image'>
-            <img src={Sheel} alt='Sheel About Image'/>
+            <img src={images[currentImageIndex]} alt={`Sheel About Image ${currentImageIndex + 1}`} />
+            {/* <img src={Sheel} alt='Sheel About Image' /> */}
           </div>
         </div>
 
@@ -37,11 +50,12 @@ export const About = () => {
               </article>
 
           </div>
-
+<div className='about__text'>
           <p>
           I'm Sheel Patel, a dedicated and versatile Computer Science student at Toronto Metropolitan University. Proficient in programming languages like Python, Java, and more, with a GPA of 3.7+. Ready to excel in computer science and contribute to innovative projects and teams. Explore my work below!
           </p>
           <a href='#content' className='btn btn-primary'>Let's Talk</a> 
+          </div>
         </div>
 
       </div>
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,19 +1,121 @@
 import React from 'react'
 import './experience.css'
+import { BsPatchCheckFill } from 'react-icons/bs'
 
 export const Experience = () => {
   return (
     <section id='experience'>
-      <h5>What Skills that I Have</h5>
+      <h5>The Skills that I Have</h5>
       <h2>Experience</h2>
 
       <div className='container experience__container'>
-      
-      </div>
 
+
+        <div className='experience__item'>
+          <h3>Frontend Development</h3>
+          <div className='experience__content'>
+          <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+              <h4>HTML</h4>
+              <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+              <h4>CSS</h4>
+              <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <h4>JavaScript</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <h4>BootStrap</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+              <h4>TailWind</h4>
+              <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div><h4>React</h4>
+              <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+
+        <div className='experience__item'>
+          <h3>Courses</h3>
+          <div className='experience__content'>
+          <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+              <h4>HTML</h4>
+              <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+              <h4>CSS</h4>
+              <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+              <h4>JavaScript</h4>
+              <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+              <h4>BootStrap</h4>
+              <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+              <h4>TailWind</h4>
+              <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+              <h4>React</h4>
+              <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+
+
+      </div>
     </section>
   )
-  
 }
 
-export default Experience 
+export default Experience
