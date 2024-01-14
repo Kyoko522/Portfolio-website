a3f134a04936acafbda3e3653997c7dc34b641db
Sheel Patel <115194165+Kyoko522@users.noreply.github.com>
2024-01-14 17:14:53 -0500

    project added

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './portfolio.css'
+import IMG0 from '../../assets/food_management.png'
 import IMG1 from '../../assets/portfolio.png'
 import IMG2 from '../../assets/icon.png'
 import IMG3 from '../../assets/TaskTonic.png'
@@ -9,38 +10,43 @@ import IMG5 from '../../assets/Flashcards-pic.png'
 const data = [
   {
     id: 1,
+    image: IMG0,
+    title: 'Food Management App',
+    github: 'https://github.com/Kyoko522/Food-Management'
+  },
+  {
+    id: 2,
     image: IMG1,
     title: 'Personal Portfolio',
     github: 'https://github.com/Kyoko522/react-portfolio',
     demo: 'still need to host to the web'
   },
 {
-  id: 2,
+  id: 3,
   image: IMG2,
   title: '3D Animated Portfolio',
   github: 'https://github.com/Kyoko522/Sheel-3D-Portfolio/tree/main',
   demo: 'https://sheel-portfolio.netlify.app/'
 },
 {
-  id: 3,
+  id: 4,
   image: IMG3,
   title: 'TaskTonic',
   github: 'https://github.com/Kyoko522/TaskTonic',
   demo: 'https://tasktonic.netlify.app/',
 },
 {
-  id: 4,
+  id: 5,
   image: IMG4,
   title: 'Minesweeper',
   github: 'https://github.com/Kyoko522/MineSweeper-Game'
 },
 {
-  id: 5, 
+  id: 6, 
   image: IMG5,
   title: 'Python Flashcards',
   github: 'https://github.com/Kyoko522/Flash_Cards'
 },
-
 
 ]
 export const Portfolio = () => {
